b7178d9cf6164d1919cbe04ae691502a17f27e55
Lalatendu Mohanty <lmohanty@redhat.com>
2016-02-11 00:36:07 +0530

    Adding synopsis
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

--- lib/command.rb
+++ lib/command.rb
@@ -1,6 +1,10 @@
-class Command < Vagrant.plugin(2, :command)
-  def execute
-    @env.ui.info("Hello World")
+module Helloworld
+  class Command < Vagrant.plugin(2, :command)
+    def self.synopsis
+        'prints helloworld'
+    end
+    def execute
+      @env.ui.info("Hello World")
+    end
   end
 end
-
